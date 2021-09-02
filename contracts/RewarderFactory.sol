51843abb6f7e132f8f9efc06849c3b015fe825c3
d1ll0n <dillonkellar@protonmail.com>
2021-09-02 02:16:29 -0500

    Use create2 deployment

--- contracts/RewarderFactory.sol
+++ contracts/RewarderFactory.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.7.6;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./libraries/CloneLibrary.sol";
 import "./DummyERC20.sol";
 import "./NDXRewarderSingle.sol";
 
 
-contract RewarderFactory {
+contract RewarderFactory is Ownable() {
   using CloneLibrary for address;
 
   IMasterChefV2 public constant masterChefV2 = IMasterChefV2(0xEF0881eC094552b2e128Cf945EF17a6752B4Ec5d);
@@ -20,12 +21,23 @@ contract RewarderFactory {
     ndxRewarderSingleImplementation = address(new NDXRewarderSingle());
   }
 
-  function deployRewarder(uint128 masterChefPid) external {
-    address stakingToken = masterChefV2.lpToken(masterChefPid);
-    require(rewarderByStakingToken[stakingToken] == address(0), "Rewarder exists");
-    address dummyToken = dummyERC20Implementation.createClone();
-    address rewarder = ndxRewarderSingleImplementation.createClone();
+  function getDummyTokenAddress(address stakingToken) public view returns (address) {
+    return dummyERC20Implementation.getCloneAddress(keccak256(abi.encode(stakingToken)));
+  }
+
+  function getRewarderAddress(address stakingToken) public view returns (address) {
+    return ndxRewarderSingleImplementation.getCloneAddress(keccak256(abi.encode(stakingToken)));
+  }
+
+  function deployRewarder(
+    address stakingToken,
+    uint128 multiTokenStakingPid
+  ) external onlyOwner {
+    bytes32 salt = keccak256(abi.encode(stakingToken));
+    address rewarder = ndxRewarderSingleImplementation.createClone(salt);
+    address dummyToken = dummyERC20Implementation.createClone(salt);
+    rewarderByStakingToken[stakingToken] = rewarder;
     DummyERC20(dummyToken).setRewarder(rewarder);
-    NDXRewarderSingle(rewarder).init(dummyToken, masterChefPid);
+    NDXRewarderSingle(rewarder).initMTS(stakingToken, dummyToken, multiTokenStakingPid);
   }
 }
